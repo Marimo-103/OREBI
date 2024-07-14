cbcee6d36bf81e3029e96f32eccbe5575923b299
Marimo-103 <m.2091hasan@gmail.com>
2024-07-14 20:21:32 +0600

    0.5%

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Container from '../Layers/Container'
+
+const Navbar = () => {
+  return (
+    <div>
+        <Container className='flex'>
+            <div className="logo">logo</div>
+            <div className="menu">menu</div>
+        </Container>
+    </div>
+  )
+}
+
+export default Navbar
