125a65b79f32b7b26a2d9e3c3daea990e757f4f9
Marimo-103 <m.2091hasan@gmail.com>
2024-08-26 21:17:17 +0600

    adds added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,61 @@
 import React from 'react'
 import IntroBanner from '../Components/IntroBanner'
+import { PiNumberTwoBold } from 'react-icons/pi'
+import Container from '../Layers/Container'
+import { FaTruck } from 'react-icons/fa'
+import { TbReload } from 'react-icons/tb'
+import add01 from '/Home Images/Ad_1.png'
+import add02 from '/Home Images/Ad_2.png'
+import add03 from '/Home Images/Ad_3.png'
+
 
 const Home = () => {
   return (
     <div>
+      {/* intro banner */}
+      <section id="intro">
+      <div className='w-full'>
       <IntroBanner />
-      <div className="flex my-20"></div>
+      </div>
+      <Container className="flex items-center justify-between w-full md:py-[22px] py-2 md:px-0 px-2">
+        <div className="one flex items-center md:gap-x-4 gap-x-[2px]">
+        <PiNumberTwoBold className='text-[#262626] md:text-base text-[10px]'/>
+        <p className='md:text-base text-[10px] text-[#6D6D6D] whitespace-nowrap'>
+        Two years warranty
+        </p>
+        </div>
+        <div className="two flex items-center md:gap-x-4 gap-x-[2px]">
+        <FaTruck className='text-[#262626] md:text-base text-[10px]'/>
+        <p className='md:text-base text-[10px] text-[#6D6D6D] whitespace-nowrap'>
+        Free Shipping
+        </p>
+        </div>
+        <div className="three flex items-center md:gap-x-4 gap-x-[2px]">
+        <TbReload className='text-[#262626] md:text-base text-[10px] rotate-90'/>
+        <p className='md:text-base text-[10px] text-[#6D6D6D] whitespace-nowrap'>
+        Return policy in 30 days
+        </p>
+        </div>
+      </Container>
+      </section>
+
+      {/* adds */}
+      <section id="adds" className='md:py-[174px] py-8 md:px-0 px-2'>
+        <Container className='flex md:gap-10 gap-3'>
+          <div className="add_1 md:w-[780px] md:h-[780px] w-[176px] h-[176px]">
+            <img src={add01} alt="" />
+          </div>
+          <div className="flex flex-col md:gap-10 gap-3">
+            <div className="add_1 md:w-[780px] md:h-[370px] w-[176px] h-[78px]">
+              <img src={add02} alt="" />
+            </div>
+            <div className="add_1 md:w-[780px] md:h-[370px] w-[176px] h-[78px]">
+              <img src={add03} alt="" />
+            </div>
+          </div>
+        </Container>
+      </section>
+      
     </div>
   )
 }
