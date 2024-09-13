16cf602defb008c37f492e16a67eb42ccc9947bf
Marimo-103 <m.2091hasan@gmail.com>
2024-09-13 21:15:42 +0600

    Sign up page complete

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Container from '../Layers/Container'
 import BreadCrumbs from '../Layers/BreadCrumbs'
 import { MdErrorOutline } from 'react-icons/md'
+import { BsFillEyeFill, BsFillEyeSlashFill } from 'react-icons/bs'
 
 const SignUp = () => {
 
@@ -45,6 +46,40 @@ const SignUp = () => {
         setTelError(false)
     }
 
+    // Adress1 states
+    const [adress1, setAdress1] = useState()
+    const [adress1Error, setAdress1Error] = useState(false)
+
+    let manageAdress1 = (element) => {
+        let adress1Value = element.target.value
+        setAdress1(adress1Value)
+        setAdress1Error(false)
+    }
+
+    // City states
+    const [city, setCity] = useState()
+    const [cityError, setCityError] = useState(false)
+
+    let manageCity = (element) => {
+        let cityValue = element.target.value
+        setCity(cityValue)
+        setCityError(false)
+    }
+
+    // Post Code states
+    const [post, setPost] = useState()
+    const [postError, setPostError] = useState(false)
+
+    let managePost = (element) => {
+        let postValue = element.target.value
+        setPost(postValue)
+        setCityError(false)
+    }
+
+    // Show / Hide Password
+    const [eye1, setEye1] = useState(true)
+    const [eye2, setEye2] = useState(true)
+
     // Password state
     const [password, setPassword] = useState()
     const [passwordError, setPasswordError] = useState(false)
@@ -55,16 +90,30 @@ const SignUp = () => {
         setPassword(passwordValue)
         setPasswordError(false)
         setPassCondition('')
+        if (!/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/.test(password)) {
+            setPassCondition('Password must contain one digit from 1 to 9, one lowercase letter, one uppercase letter, one special character, no space, and it must be 8-16 characters long.')
+        }else {
+            setPassCondition('')
+        }
+        
     }
 
     // Retype Password state
     const [rePassword, setRePassword] = useState()
     const [rePasswordError, setRePasswordError] = useState(false)
+    const [passCondition2, setPassCondition2] = useState()
 
     let manageRePassword = (element) => {
         let rePasswordValue = element.target.value
         setRePassword(rePasswordValue)
         setRePasswordError(false)
+        setPassCondition2('')
+        if (!/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/.test(password)) {
+            setPassCondition2('Password must contain one digit from 1 to 9, one lowercase letter, one uppercase letter, one special character, no space, and it must be 8-16 characters long.')
+        }else {
+            setPassCondition2('')
+        }
+        
     }
 
     // Submit button function
@@ -81,15 +130,35 @@ const SignUp = () => {
         if (!tel) {
             setTelError(true)
         }
+        if (!adress1) {
+            setAdress1Error(true)
+        }
+        if (!city) {
+            setCityError(true)
+        }
+        if (!post) {
+            setPostError(true)
+        }
         if (!password) {
             setPasswordError(true)
-        }else if (!/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/.test(password)) {
-            setPassCondition('Password must contain one digit from 1 to 9, one lowercase letter, one uppercase letter, one special character, no space, and it must be 8-16 characters long.')
         }
         if (!rePassword) {
             setRePasswordError(true)
         }
+        if (password  !== rePassword) {
+            setPassCondition2('Password and Retype Password must be the same')
+        }
     }
+
+    // Division array 
+    let division = ['Barishal', 'Chattogram', 'Dhaka', 'Khulna', 'Rajshahi', 'Rangpur', 'Mymensingh', 'Sylhet']
+
+    // District array
+    let district = [
+        "Barguna", "Barishal", "Bhola", "Jhalokathi", "Patuakhali", "Pirojpur", "Bandarban", "Brahmanbaria", "Chandpur", "Chattogram", "Cumilla", "Cox's Bazar", "Feni", "Khagrachari", "Lakshmipur", "Noakhali", "Rangamati", "Dhaka", "Faridpur", "Gazipur", "Gopalganj", "Kishoreganj", "Madaripur", "Manikganj", "Munshiganj", "Narayanganj", "Narsingdi", "Rajbari", "Shariatpur", "Tangail", "Bagerhat", "Chuadanga", "Jashore", "Jhenaidah", "Khulna", "Kushtia", "Magura", "Meherpur", "Narail", "Satkhira",
+        "Jamalpur", "Mymensingh", "Netrokona", "Sherpur", "Bogra", "Joypurhat", "Naogaon", "Natore", "Chapai Nawabganj", "Pabna", "Rajshahi", "Sirajganj", "Dinajpur", "Gaibandha", "Kurigram", "Lalmonirhat", "Nilphamari", "Panchagarh", "Rangpur", "Thakurgaon",
+        "Habiganj", "Moulvibazar", "Sunamganj", "Sylhet"
+    ]
 
   return (
     <div className='md:px-0 px-2'>
@@ -176,29 +245,85 @@ const SignUp = () => {
                 New Customer
                 </h2>
                 <div className="userInfo flex flex-wrap md:gap-x-10 sm:justify-between md:justify-normal gap-y-6">
-                    <div className="fname flex flex-col md:gap-[10px] gap-[6px] w-full sm:w-[47%] md:w-[508px]">
+                    <div className="adress1 flex flex-col md:gap-[10px] gap-[6px] w-full sm:w-[47%] md:w-[508px] relative">
                         <label htmlFor="">
                         Address 1
                         </label>
-                        <input type="text" placeholder='example: 4279 Zboncak Port Suite 6212' className='md:pb-[17px] pb-2 text-sm text-[#262626] w-full border-b outline-none'/>
+                        <input onChange={manageAdress1} value={adress1} type="text" placeholder='example: 4279 Zboncak Port Suite 6212' className='md:pb-[17px] pb-2 text-sm text-[#262626] w-full border-b outline-none'/>
+                        {
+                            adress1Error && (
+                                <>
+                                <p className='md:text-xs text-[10px] tracking-wide font-medium text-red-700 absolute md:bottom-[6px] bottom-0 left-0'>
+                                This field is Required 
+                                </p>
+                                <MdErrorOutline className='text-xs text-red-700 absolute bottom-[6px] right-0'/>
+                                </>
+                            )
+                        }
                     </div>
-                    <div className="lname flex flex-col md:gap-[10px] gap-[6px] w-full sm:w-[47%] md:w-[508px]">
+                    <div className="adress2 flex flex-col md:gap-[10px] gap-[6px] w-full sm:w-[47%] md:w-[508px]">
                         <label htmlFor="">
                         Address 2
                         </label>
-                        <input type="text" placeholder='-' className='md:pb-[17px] pb-2 text-sm text-[#262626] w-full border-b outline-none'/>
+                        <input type="text" placeholder='- ( Optional )' className='md:pb-[17px] pb-2 text-sm text-[#262626] w-full border-b outline-none'/>
                     </div>
-                    <div className="fname flex flex-col md:gap-[10px] gap-[6px] w-full sm:w-[47%] md:w-[508px]">
+                    <div className="city flex flex-col md:gap-[10px] gap-[6px] w-full sm:w-[47%] md:w-[508px] relative">
                         <label htmlFor="">
                         City
                         </label>
-                        <input type="text" placeholder='Your City' className='md:pb-[17px] pb-2 text-sm text-[#262626] w-full border-b outline-none'/>
+                        <input onChange={manageCity} value={city} type="text" placeholder='Your City' className='md:pb-[17px] pb-2 text-sm text-[#262626] w-full border-b outline-none'/>
+                        {
+                            cityError && (
+                                <>
+                                <p className='md:text-xs text-[10px] tracking-wide font-medium text-red-700 absolute md:bottom-[6px] bottom-0 left-0'>
+                                This field is Required 
+                                </p>
+                                <MdErrorOutline className='text-xs text-red-700 absolute bottom-[6px] right-0'/>
+                                </>
+                            )
+                        }
                     </div>
-                    <div className="fname flex flex-col md:gap-[10px] gap-[6px] w-full sm:w-[47%] md:w-[508px]">
+                    <div className="postcode flex flex-col md:gap-[10px] gap-[6px] w-full sm:w-[47%] md:w-[508px] relative">
                         <label htmlFor="">
                         Post Code
                         </label>
-                        <input type="text" placeholder='example: 05228' className='md:pb-[17px] pb-2 text-sm text-[#262626] w-full border-b outline-none'/>
+                        <input onChange={managePost} value={post} type="text" placeholder='example: 05228' className='md:pb-[17px] pb-2 text-sm text-[#262626] w-full border-b outline-none'/>
+                        {
+                            postError && (
+                                <>
+                                <p className='md:text-xs text-[10px] tracking-wide font-medium text-red-700 absolute md:bottom-[6px] bottom-0 left-0'>
+                                This field is Required 
+                                </p>
+                                <MdErrorOutline className='text-xs text-red-700 absolute bottom-[6px] right-0'/>
+                                </>
+                            )
+                        }
+                    </div>
+                    <div className="division flex flex-col md:gap-[10px] gap-[6px] w-full sm:w-[47%] md:w-[508px] relative">
+                        <label htmlFor="">
+                        Division
+                        </label>
+                        <select name="" id="" className='md:pb-[17px] pb-2 text-sm text-[#767676] w-full border-b outline-none bg-white'>
+                            <option value="">Please select</option>
+                            {
+                                division.map((item, index) => (
+                                    <option key={index} value="">{item}</option>
+                                ))
+                            }
+                        </select>
+                    </div>
+                    <div className="district flex flex-col md:gap-[10px] gap-[6px] w-full sm:w-[47%] md:w-[508px] relative">
+                        <label htmlFor="">
+                        District
+                        </label>
+                        <select name="" id="" className='md:pb-[17px] pb-2 text-sm text-[#767676] w-full border-b outline-none bg-white'>
+                            <option value="">Please select</option>
+                            {
+                                district.map((item, index) => (
+                                    <option key={index} value="">{item}</option>
+                                ))
+                            }
+                        </select>
                     </div>
                 </div>
             </Container>
@@ -207,12 +332,14 @@ const SignUp = () => {
                 Your Password
                 </h2>
                 <div className="userInfo flex flex-wrap md:gap-x-10 sm:justify-between md:justify-normal gap-y-6">
-                    <div className="fname flex flex-col md:gap-[10px] gap-[6px] w-full sm:w-[47%] md:w-[508px] relative">
+                    <div className="Pass flex flex-col md:gap-[10px] gap-[6px] w-full sm:w-[47%] md:w-[508px] relative">
                         <label htmlFor="">
                         Password
                         </label>
-                        <input onChange={managePassword} value={password} type="text" placeholder='Password' className='md:pb-[17px] pb-2 text-sm text-[#262626] w-full border-b outline-none'/>
-                        <p className='md:text-xs text-[10px] tracking-wide font-medium text-red-700 absolute top-full left-0'>
+                        <input onChange={managePassword} value={password} type={eye1 ? 'password' : 'text'} placeholder='Password' className='md:pb-[17px] pb-2 text-sm text-[#262626] w-full border-b outline-none'/>
+                        <BsFillEyeFill onClick={() => setEye1(!eye1)} className={`absolute bottom-[17px] right-0 ${ eye1 ? 'opacity-0' : 'opacity-100'}`}/>
+                        <BsFillEyeSlashFill onClick={() => setEye1(!eye1)} className={`absolute bottom-[17px] right-0 ${ eye1 ? 'opacity-100' : 'opacity-0'}`}/>
+                        <p className='md:text-xs text-[9px] md:tracking-wide font-medium text-red-700 absolute top-full left-0'>
                             {passCondition}
                         </p>
                         {
@@ -226,13 +353,15 @@ const SignUp = () => {
                             )
                         }
                     </div>
-                    <div className="lname flex flex-col md:gap-[10px] gap-[6px] w-full sm:w-[47%] md:w-[508px] relative">
+                    <div className="rePass flex flex-col md:gap-[10px] gap-[6px] w-full sm:w-[47%] md:w-[508px] relative">
                         <label htmlFor="">
                         Repeat Password
                         </label>
-                        <input onChange={manageRePassword} value={rePassword} type="text" placeholder='Repeat Password' className='md:pb-[17px] pb-2 text-sm text-[#262626] w-full border-b outline-none'/>
-                        <p className='md:text-xs text-[10px] tracking-wide font-medium text-red-700 absolute top-full left-0'>
-                            {passCondition} 
+                        <input onChange={manageRePassword} value={rePassword} type={eye2 ? 'password' : 'text'} placeholder='Repeat Password' className='md:pb-[17px] pb-2 text-sm text-[#262626] w-full border-b outline-none'/>
+                        <BsFillEyeFill onClick={() => setEye2(!eye2)} className={`absolute bottom-[17px] right-0 ${ eye2 ? 'opacity-0' : 'opacity-100'}`}/>
+                        <BsFillEyeSlashFill onClick={() => setEye2(!eye2)} className={`absolute bottom-[17px] right-0 ${ eye2 ? 'opacity-100' : 'opacity-0'}`}/>
+                        <p className='md:text-xs text-[9px] md:tracking-wide font-medium text-red-700 absolute top-full left-0'>
+                            {passCondition2} 
                         </p>
                         {
                             rePasswordError && (
